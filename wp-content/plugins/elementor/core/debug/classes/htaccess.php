f1a2d7e4c1cfb999ed1b656196c5b9e8300ce965
ehsankhashi <ehsankhashi071@gmail.com>
2023-01-19 13:40:59 -0500

    first

---
+++ wp-content/plugins/elementor/core/debug/classes/htaccess.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;ElementorCoreDebugClassesInspection_Base&quot; not found in C:xampphtdocswp-contentpluginselementorcoredebugclasseshtaccess.php:7
+Stack trace:
+#0 {main}
+  thrown in <b>C:xampphtdocswp-contentpluginselementorcoredebugclasseshtaccess.php</b> on line <b>7</b><br>
