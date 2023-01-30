41641de3dca05df3ac550feebd8af2854fc91eec
ehsankhashi <ehsankhashi071@gmail.com>
2023-01-30 13:23:05 -0500

    new

---
+++ wp-content/plugins/nextend-smart-slider3-pro/Nextend/Framework/Asset/Fonts/Google/Asset.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;Nextend\Framework\Asset\AbstractAsset&quot; not found in C:\xampp\htdocs\wp-content\plugins\nextend-smart-slider3-pro\Nextend\Framework\Asset\Fonts\Google\Asset.php:11
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\wp-content\plugins\nextend-smart-slider3-pro\Nextend\Framework\Asset\Fonts\Google\Asset.php</b> on line <b>11</b><br>
